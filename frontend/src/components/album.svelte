0f7611fa453a6aab2a4c62cc15b87202e1fbf003
benjamin.johnson <benjamin.johnson@madetech.com>
2023-02-27 10:47:28 +0000

    added some temporary css to album component

--- frontend/src/components/album.svelte
+++ frontend/src/components/album.svelte
@@ -2,4 +2,14 @@
     let albumArt = 'https://upload.wikimedia.org/wikipedia/en/f/f8/10000Days.jpg';
 </script>
 
-<img src="{albumArt}" alt="album artwork" />
+<div class="album-container">
+    <img src="{albumArt}" alt="album artwork" />
+</div>
+
+
+<style>
+    .album-container {
+        width: 100%;
+        border: #FF0000 solid 1px;
+    }
+</style>
